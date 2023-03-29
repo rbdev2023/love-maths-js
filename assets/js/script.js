afbf9d18598bc5a3bba54611e7e8c9e3022ed14d
rbdev2023 <rbdev2023@outlook.com>
2023-03-29 13:07:14 +0100

    Add structure to JS functions for the game.

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame(){
+
+}
+
+function checkScore(){
+    
+}
+
+function checkCorrectAnswer(){
+    
+}
+
+function incrementCorrectAnswer(){
+    
+}
+
+function incrementIncorrectAnswer(){
+    
+}
+
+function displayAdditionQuestion(){
+    
+}
+
+function displaySubtractQuestion(){
+    
+}
+
+function displayMultiplyQuestion(){
+    
+}
