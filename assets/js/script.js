0729f2c66417eb68cea1c48b82b837f74072a8d5
rbdev2023 <rbdev2023@outlook.com>
2023-03-29 15:08:59 +0100

    Display addition question to the user.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,18 +8,27 @@ document.addEventListener('DOMContentLoaded', function(){
                 alert('You clicked submit')
             } else{
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`)
+                runGame(gameType);
             }
         })
     }
+
+    runGame('addition')
 })
 
 /**
  * Displays 2 random numbers
  */
-function runGame(){
+function runGame(gameType){
     let num1 = Math.floor(Math.random() * 25 + 1)
     let num2 = Math.floor(Math.random() * 25 + 1)
+
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2)
+    } else{
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkScore(){
@@ -38,8 +47,10 @@ function incrementIncorrectAnswer(){
     
 }
 
-function displayAdditionQuestion(){
-    
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1
+    document.getElementById('operand2').textContent = operand2
+    document.getElementById('operator').textContent = '+'
 }
 
 function displaySubtractQuestion(){
